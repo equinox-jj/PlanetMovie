ee467d6cd4156dadd38a6de20c811999c95066dc
equinox-jj <joshua.jeremia@rocketmail.com>
2022-05-03 23:18:09 +0700

    Final.

--- app/src/main/java/com/planetmovie/data/local/database/MovieTypeConverter.kt
+++ app/src/main/java/com/planetmovie/data/local/database/MovieTypeConverter.kt
@@ -4,7 +4,6 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.planetmovie.data.remote.model.MovieResponse
-import com.planetmovie.data.remote.model.MovieResult
 
 class MovieTypeConverter {
 
@@ -18,17 +17,6 @@ class MovieTypeConverter {
     @TypeConverter
     fun stringToMovieData(data: String): MovieResponse {
         val listType = object : TypeToken<MovieResponse>() {}.type
-        return gson.fromJson(data, listType)
-    }
-
-    @TypeConverter
-    fun movieResultToString(movieResult: MovieResult): String {
-        return gson.toJson(movieResult)
-    }
-
-    @TypeConverter
-    fun stringToMovieResult(data: String): MovieResult {
-        val listType = object : TypeToken<MovieResult>() {}.type
         return gson.fromJson(data, listType)
     }
 
